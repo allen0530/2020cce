eb6760833ccaba12deb2e94bd7de92bacf7fd7dd
allen0530 <79676832+allen0530@users.noreply.github.com>
2021-05-07 10:26:34 +0800

    Add files via upload

--- 0916109501.cpp
+++ 0916109501.cpp
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+int a[10]={4,8,3,7,5,2,9,1,6,10};
+int compare(const void *p1,const void *p2)
+{
+    int d1=* (int*)p1;
+    int d2=* (int*)p2;
+    if(d1>d2) return 1;
+    if(d1==d2) return 0;
+    if(d1<d2) return -1;
+
+}
 int main()
 {
-    char line[10]="decline";
-    char line2[10]={'p','r','o','p','e','r','\0'};
-    printf("%s\n",line);
-    printf("%s\n",line2);
-    char line3[]="majority";
-    printf("%s\n",line3);
-    char line4[]={'m','a','j','o','r','i','t','y'};
-    printf("你看看你看看,現在印出來的line4:==%s==\n",line4);
 
-
-
-
-
-
-
+        qsort(a,10,sizeof(int),compare);
+        for(int i=0;i<10;i++){
+            printf("%d ",a[i]);
+        }
 }
